b1bd86844278d22ccc075843ca2905d9a253864e
AndreasArnholm <andreas@arnholm.org>
2021-03-11 13:15:31 +0100

    added missing script from previous commit

--- ros2/build.sh
+++ ros2/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # test: bash build.sh binary TCP test 
-# prod: bash build.sh binary TCP  
+# prod: bash build.sh binary TCP prod
 
 build_type=$1
 connection_type=$2
+connection_type=$( echo "$connection_type" | tr '[a-z]' '[A-Z]' )
 run_type=$3
 
 # Prod parameters as default 
@@ -12,7 +13,9 @@ lbr_port=30005
 kmp_port=30002
 kmr_ip="172.31.1.69'"
 opc_ua_domain="andrcar-master.ivt.ntnu.no"
-
+robot="KMR"
+lbr_id=1
+kmp_id=1
 
 if [ $build_type = 'source' ]
 then
@@ -34,33 +37,18 @@ then
     echo "Running in production mode"
 fi
 
-yq -y '.connection_params.ros__parameters.connection_type = ${connection_type}' \
-    kmr_communication/kmr_communication/config/bringup_test.yaml \
-    | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
+sed "/^\([[:space:]]*connection_type: \).*/s//\1\'$connection_type\'/" kmr_communication/kmr_communication/config/bringup_base.yaml > kmr_communication/kmr_communication/config/bringup_run.yaml
+sed -i "/^\([[:space:]]*robot: \).*/s//\1\'$robot\'/" kmr_communication/kmr_communication/config/bringup_run.yaml
+sed -i "/^\([[:space:]]*domain: \).*/s//\1\'$opc_ua_domain\'/" kmr_communication/kmr_communication/config/bringup_run.yaml
 
-# cat kmr_communication/kmr_communication/config/bringup_test.yaml \
-#     | yq -y '.connection_params.ros__parameters.connection_type = $v' --arg v $connection_type \
-#     | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
+sed -i 's/lbr_id/'$lbr_id'/' kmr_communication/kmr_communication/config/bringup_run.yaml
+sed -i 's/kmp_id/'$kmp_id'/' kmr_communication/kmr_communication/config/bringup_run.yaml
 
-# cat kmr_communication/kmr_communication/config/bringup_test.yaml \
-#     | yq -y '.lbr_command_node.ros__parameters.port = $v' --arg v $lbr_port \
-#     | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
+sed -i 's/lbr_port/'$lbr_port'/' kmr_communication/kmr_communication/config/bringup_run.yaml
+sed -i 's/kmp_port/'$kmp_port'/' kmr_communication/kmr_communication/config/bringup_run.yaml
 
-# cat kmr_communication/kmr_communication/config/bringup_test.yaml \
-#     | yq -y '.lbr_command_node.ros__parameters.kmr_ip = $v' --arg v $kmr_ip \
-#     | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
-
-# cat kmr_communication/kmr_communication/config/bringup_test.yaml \
-#     | yq -y '.kmp_command_node.ros__parameters.port = $v' --arg v $kmp_port \
-#     | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
-
-# cat kmr_communication/kmr_communication/config/bringup_test.yaml \
-#     | yq -y '.kmp_command_node.ros__parameters.kmr_ip = $v' --arg v $kmr_ip \
-#     | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
-
-# cat kmr_communication/kmr_communication/config/bringup_test.yaml \
-#     | yq -y '.hybrid_node.ros__parameters.domain = $v' --arg v $opc_ua_domain \
-#     | sponge kmr_communication/kmr_communication/config/bringup_test.yaml
+sed -i 's/lbr_ip/'$kmr_ip'/' kmr_communication/kmr_communication/config/bringup_run.yaml
+sed -i 's/kmp_ip/'$kmr_ip'/' kmr_communication/kmr_communication/config/bringup_run.yaml
 
 colcon build --symlink-install
 source install/setup.bash 
